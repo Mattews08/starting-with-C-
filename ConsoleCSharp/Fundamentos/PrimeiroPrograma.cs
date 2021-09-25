0480dbe2c990c4d270e2c854bf57ffdaca46e939
Matheus Lima <l.matheus08@outlook.com>
2021-09-24 23:05:56 -0400

    starting with C#

---
+++ ConsoleCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleCSharp.Fundamentos
+{
+    class PrimeiroPrograma
+    {
+        static void Main()
+        {
+            Console.Write("Primeiro ");
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou!");
+        }
+    }
+}
---
+++ ConsoleCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            {
+                Console.WriteLine("Hello World 2");
+            }
+        }
+    }
+}
